07c7f6d422f41fe62538cb9c6d7880f42bb4c68f
jim-nnamdi <jimsamuel50@gmail.com>
2023-01-02 11:46:21 +0100

    Cryptocurrencies data integrations

--- backend/finance/cryptos.go
+++ backend/finance/cryptos.go
@@ -1,14 +1,17 @@
 package finance
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"go.uber.org/zap"
 )
 
 type CryptoData interface {
-	GetAllCryptoData()
+	GetAllCryptoData() (*AllCrypto, error)
+	GetLiveCryptoData() (*LiveData, error)
+	ConvertCrypto(coinfrom string, cointo string, amount int) (float64, error)
 }
 
 var _ CryptoData = &Cryptos{}
@@ -24,6 +27,20 @@ type CryptoResponse struct {
 	} `json:"crypto"`
 }
 
+type AllCrypto struct {
+	Success bool        `json:"success"`
+	Crypto  interface{} `json:"crypto"`
+}
+
+type LiveData struct {
+	Timestamp int `json:"timestamp"`
+
+	// currency we want to display
+	// the current rates in
+	Target string             `json:"target"`
+	Rates  map[string]float64 `json:"rates"`
+}
+
 func NewCryptos(logger *zap.Logger, sclient RequestClient) *Cryptos {
 	return &Cryptos{
 		logger:  logger,
@@ -31,11 +48,52 @@ func NewCryptos(logger *zap.Logger, sclient RequestClient) *Cryptos {
 	}
 }
 
-func (cx *Cryptos) GetAllCryptoData() {
-	req, err := cx.sclient.MakeGetRequest(http.MethodGet, "http://api.coinlayer.com/api/list?access_key=09e5664c792fbfa119e714e95095eedc")
+func (cs *Cryptos) GetAllCryptoData() (*AllCrypto, error) {
+	req, err := cs.sclient.MakeGetRequest(http.MethodGet, "http://api.coinlayer.com/api/list?access_key=09e5664c792fbfa119e714e95095eedc")
 	if err != nil {
-		cx.logger.Debug("error fetching data", zap.Any("error", err))
-		return
+		cs.logger.Debug("error fetching data", zap.Any("error", err))
+		return nil, err
 	}
-	fmt.Print(req)
+	var val AllCrypto
+	_ = json.Unmarshal(req, &val)
+	return &val, nil
+}
+
+func (cs *Cryptos) GetLiveCryptoData() (*LiveData, error) {
+	req, err := cs.sclient.MakeGetRequest(http.MethodGet, "http://api.coinlayer.com/api/live?access_key=09e5664c792fbfa119e714e95095eedc")
+	if err != nil {
+		cs.logger.Debug("error fetching data", zap.Any("error", err))
+		return nil, err
+	}
+	var val LiveData
+	_ = json.Unmarshal(req, &val)
+	return &val, nil
+}
+
+func (cs *Cryptos) ConvertCrypto(coinfrom string, cointo string, amount int) (float64, error) {
+	req, err := cs.sclient.MakeGetRequest(http.MethodGet, "http://api.coinlayer.com/api/live?access_key=09e5664c792fbfa119e714e95095eedc")
+	if err != nil {
+		cs.logger.Debug("error fetching data", zap.Any("error", err))
+		return 0.0, err
+	}
+	var val LiveData
+	_ = json.Unmarshal(req, &val)
+	if val.Rates != nil {
+		var coinFromPrice float64
+		var coinToPrice float64
+		for k, v := range val.Rates {
+			if k == coinfrom {
+				log.Print("coinfrom price -> ", v)
+				coinFromPrice = v
+			}
+			if k == cointo {
+				log.Print("cointo price -> ", v)
+				coinToPrice = v
+			}
+		}
+		calcCoinFromAndAmount := coinFromPrice * float64(amount)
+		calcConversionPrice := calcCoinFromAndAmount / coinToPrice
+		return calcConversionPrice, nil
+	}
+	return 0.0, nil
 }
